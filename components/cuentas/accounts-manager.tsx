bf78c2337a32da1c52b7894729064a2cda01cebf
AMordelle <wilcas0207@gmail.com>
2026-04-17 21:00:49 -0600

    Fix use client directive order in accounts manager

--- components/cuentas/accounts-manager.tsx
+++ components/cuentas/accounts-manager.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 'use client';
 
+import React from 'react';
 import { useMemo, useState, useTransition } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
